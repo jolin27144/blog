350683c593118551ec382602e6864bd3feffaf53
izzy <542001917@qq.com>
2020-04-19 19:29:15 +0800

    feat: init vue-graphql

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off",
+    "graphql/template-strings": [
+      "error",
+      {
+        env: "literal",
+        projectName: "app",
+        schemaJsonFilepath: "node_modules/.temp/graphql/schema.json"
+      }
+    ]
+  },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
+
+  plugins: ["graphql"]
+};
